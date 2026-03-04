cba32552d5cf1964bb022695695e09399e4bf012
YunaiV <zhijiantianya@gmail.com>
2026-03-04 22:42:07 +0800

    ✨ feat(mes): 添加 MES 编码生成策略的单元测试

--- yudao-module-mes/src/main/java/cn/iocoder/yudao/module/mes/dal/mysql/md/autocode/MesMdAutoCodeRecordMapper.java
+++ yudao-module-mes/src/main/java/cn/iocoder/yudao/module/mes/dal/mysql/md/autocode/MesMdAutoCodeRecordMapper.java
@@ -1,9 +1,6 @@
 package cn.iocoder.yudao.module.mes.dal.mysql.md.autocode;
 
-import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
-import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
-import cn.iocoder.yudao.module.mes.controller.admin.md.autocode.vo.record.MesMdAutoCodeRecordPageReqVO;
 import cn.iocoder.yudao.module.mes.dal.dataobject.md.autocode.MesMdAutoCodeRecordDO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -14,14 +11,6 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface MesMdAutoCodeRecordMapper extends BaseMapperX<MesMdAutoCodeRecordDO> {
-
-    default PageResult<MesMdAutoCodeRecordDO> selectPage(MesMdAutoCodeRecordPageReqVO reqVO) {
-        return selectPage(reqVO, new LambdaQueryWrapperX<MesMdAutoCodeRecordDO>()
-                .eqIfPresent(MesMdAutoCodeRecordDO::getRuleId, reqVO.getRuleId())
-                .likeIfPresent(MesMdAutoCodeRecordDO::getResult, reqVO.getResult())
-                .betweenIfPresent(MesMdAutoCodeRecordDO::getCreateTime, reqVO.getCreateTime())
-                .orderByDesc(MesMdAutoCodeRecordDO::getId));
-    }
 
     default MesMdAutoCodeRecordDO selectByResult(String result) {
         return selectOne(MesMdAutoCodeRecordDO::getResult, result);
---
+++ yudao-module-mes/src/test/resources/sql/clean.sql
@@ -0,0 +1,3 @@
+DELETE FROM "mes_md_auto_code_rule";
+DELETE FROM "mes_md_auto_code_part";
+DELETE FROM "mes_md_auto_code_record";
