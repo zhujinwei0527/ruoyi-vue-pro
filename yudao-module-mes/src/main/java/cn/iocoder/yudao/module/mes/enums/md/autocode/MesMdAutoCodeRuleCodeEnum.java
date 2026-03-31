5a250de35c96b73630d514f957cf1e444cf181b7
YunaiV <zhijiantianya@gmail.com>
2026-03-31 16:03:35 +0800

    ✨ feat(mes): 更新转移调拨单编码常量名称

--- yudao-module-mes/src/main/java/cn/iocoder/yudao/module/mes/enums/md/autocode/MesMdAutoCodeRuleCodeEnum.java
+++ yudao-module-mes/src/main/java/cn/iocoder/yudao/module/mes/enums/md/autocode/MesMdAutoCodeRuleCodeEnum.java
@@ -34,7 +34,7 @@ public enum MesMdAutoCodeRuleCodeEnum {
     WM_AREA_CODE("WM_AREA_CODE", "库位编码"),
     WM_PRODUCT_SALES_CODE("WM_PRODUCT_SALES_CODE", "销售出库单编码"),
     WM_MISC_RECEIPT_CODE("WM_MISC_RECEIPT_CODE", "杂项入库单编码"),
-    WM_TRANSFER_CODE("WM_TRANSFER_CODE", "转移调拨单编码");
+    TRANSFER_CODE("TRANSFER_CODE", "转移调拨单编码");
 
     /**
      * 规则代码
